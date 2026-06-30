70b1a10d13fb18ba7c765bdb72375368971d3e07
Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>
2026-06-30 11:05:47 +0200

    feat: allow setting custom arguments for linters
    Signed-off-by: Georg Lauterbach <44545919+georglauterbach@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,23 +3,39 @@
 set -eE -u -o pipefail
 shopt -s inherit_errexit
 
-# shellcheck source=libbash
-source /etc/linters/libbash log utils
-export SCRIPT=linter
-export LOG_LEVEL=${LOG_LEVEL:-trace}
-
-readonly CONFIGURATION_DIR=/etc/linters/configuration
-
+SCRIPT=linter
+LOG_LEVEL=${LOG_LEVEL:-debug}
 SUCCESS=true
 
-libbash::log::log info 'Starting linter'
+# shellcheck source=libbash
+source /etc/linters/libbash log utils
+
+IFS=" " read -r -a ACTIONLINT_CUSTOM_ARGUMENTS \
+  <<< "${ACTIONLINT_CUSTOM_ARGUMENTS:--color -shellcheck $(command -v shellcheck)}"
+IFS=" " read -r -a EDITORCONFIG_CHECKER_CUSTOM_ARGUMENTS \
+  <<< "${EDITORCONFIG_CHECKER_CUSTOM_ARGUMENTS:--color}"
+IFS=" " read -r -a HADOLINT_CUSTOM_ARGUMENTS \
+  <<< "${HADOLINT_CUSTOM_ARGUMENTS:-}"
+IFS=" " read -r -a SHELLCHECK_CUSTOM_ARGUMENTS \
+  <<< "${SHELLCHECK_CUSTOM_ARGUMENTS:-}"
+IFS=" " read -r -a YAMLLINT_CUSTOM_ARGUMENTS \
+  <<< "${YAMLLINT_CUSTOM_ARGUMENTS:---strict --format github}"
+IFS=" " read -r -a ZIZMOR_CUSTOM_ARGUMENTS \
+  <<< "${ZIZMOR_CUSTOM_ARGUMENTS:---quiet --persona=pedantic --no-online-audits --no-progress --format github --strict-collection}"
+
+readonly CONFIGURATION_DIR=/etc/linters/configuration
+readonly ACTIONLINT_CUSTOM_ARGUMENTS           ACTIONLINT_VERSION
+readonly EDITORCONFIG_CHECKER_CUSTOM_ARGUMENTS EDITORCONFIG_CHECKER_VERSION
+readonly HADOLINT_CUSTOM_ARGUMENTS             HADOLINT_VERSION
+readonly SHELLCHECK_CUSTOM_ARGUMENTS           SHELLCHECK_VERSION
+readonly YAMLLINT_CUSTOM_ARGUMENTS             YAMLLINT_VERSION
+readonly ZIZMOR_CUSTOM_ARGUMENTS               ZIZMOR_VERSION
+
+libbash::log::log debug 'Starting linter'
 
 if ${ENABLE_ACTIONLINT:-}; then
-  # shellcheck disable=SC2154
   libbash::log::log info "Running actionlint ${ACTIONLINT_VERSION}"
-  if actionlint \
-    -color      \
-    -shellcheck "$(command -v shellcheck)" \
+  if actionlint "${ACTIONLINT_CUSTOM_ARGUMENTS[@]}" \
     -config-file "${ACTIONLINT_CONFIG:-${CONFIGURATION_DIR}/actionlint.yaml}"
   then
     libbash::log::log info 'Linting with actionlint succeeded'
@@ -30,10 +46,8 @@ if ${ENABLE_ACTIONLINT:-}; then
 fi
 
 if ${ENABLE_EDITORCONFIG_CHECKER:-}; then
-  # shellcheck disable=SC2154
   libbash::log::log info "Running editorconfig-checker ${EDITORCONFIG_CHECKER_VERSION}"
-  if ec    \
-    -color \
+  if ec "${EDITORCONFIG_CHECKER_CUSTOM_ARGUMENTS[@]}" \
     -config "${EDITORCONFIG_CHECKER_CONFIG:-${CONFIGURATION_DIR}/editorconfig_checker.json}"
   then
     libbash::log::log info 'Linting with editorconfig-checker succeeded'
@@ -44,11 +58,10 @@ if ${ENABLE_EDITORCONFIG_CHECKER:-}; then
 fi
 
 if ${ENABLE_HADOLINT:-}; then
-  # shellcheck disable=SC2154
   libbash::log::log info "Running hadolint ${HADOLINT_VERSION}"
   HADOLINT_SUCCESS=true
   while read -r FILE; do
-    if ! hadolint \
+    if ! hadolint "${HADOLINT_CUSTOM_ARGUMENTS[@]}" \
       --config "${HADOLINT_CONFIG:-${CONFIGURATION_DIR}/hadolint.yaml}" \
       "${FILE}"
     then
@@ -65,11 +78,10 @@ if ${ENABLE_HADOLINT:-}; then
 fi
 
 if ${ENABLE_SHELLCHECK:-}; then
-  # shellcheck disable=SC2154
   libbash::log::log info "Running shellcheck ${SHELLCHECK_VERSION}"
   SHELLCHECK_SUCCESS=true
   while read -r FILE; do
-    if ! shellcheck \
+    if ! shellcheck "${SHELLCHECK_CUSTOM_ARGUMENTS[@]}" \
       --rcfile="${SHELLCHECK_CONFIG:-${CONFIGURATION_DIR}/shellcheck.conf}" \
       "${FILE}"
     then
@@ -86,11 +98,8 @@ if ${ENABLE_SHELLCHECK:-}; then
 fi
 
 if ${ENABLE_YAMLLINT:-}; then
-  # shellcheck disable=SC2154
   libbash::log::log info "Running yamllint ${YAMLLINT_VERSION%-*}"
-  if yamllint       \
-    --strict        \
-    --format github \
+  if yamllint "${SHELLCHECK_CUSTOM_ARGUMENTS[@]}" \
     --config-file "${YAMLLINT_CONFIG:-${CONFIGURATION_DIR}/yamllint.yaml}" \
     .
   then
@@ -102,15 +111,8 @@ if ${ENABLE_YAMLLINT:-}; then
 fi
 
 if ${ENABLE_ZIZMOR:-}; then
-  # shellcheck disable=SC2154
   libbash::log::log info "Running zizmor ${ZIZMOR_VERSION}"
-  if zizmor             \
-    --quiet             \
-    --persona=pedantic  \
-    --no-online-audits  \
-    --no-progress       \
-    --format github     \
-    --strict-collection \
+  if zizmor "${ZIZMOR_CUSTOM_ARGUMENTS[@]}" \
     --config "${ZIZMOR_CONFIG:-${CONFIGURATION_DIR}/zizmor.yaml}" \
     .
   then
